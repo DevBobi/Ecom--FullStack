22f33dbf7ec37d3869732bb5edc41ae9c03f261a
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-04-17 02:41:57 +0600

    error handled & apifeatures added

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,9 +4,10 @@ const connectDatabase = () => {
 
     mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`MongoDB connected with server ${data.connection.host}`)
-    }).catch((err) => {
-        console.log(err)
     })
+    // .catch((err) => {
+    //     console.log(err)   /* Don't need this anymore bc we handled Unhandled Promise Rejection Error on server */
+    // })
 
 }
 
