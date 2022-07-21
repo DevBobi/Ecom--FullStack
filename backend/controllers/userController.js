0bf62e9adcd6c5f4d30862f7206741fad8f0cf41
devbobi <md.saifurrahaman.cse@gmail.com>
2022-07-22 01:19:10 +0600

    error handled also user detail added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,8 @@ const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
 const sendToken = require("../utils/jwtToken");
-const sendEmail = require("../utils/sendEmail")
+const sendEmail = require("../utils/sendEmail");
+const crypto = require("crypto");
 
 // Register a user
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -23,26 +24,28 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
 // Login User
 exports.loginUser = catchAsyncErrors(async (req, res, next) => {
-
     const { email, password } = req.body;
-
+  
+    // checking if user has given password and email both
+  
     if (!email || !password) {
-        return next(new ErrorHandler("Please Enter Email & Passowrd", 400));
+      return next(new ErrorHandler("Please Enter Email & Password", 400));
     }
-
+  
     const user = await User.findOne({ email }).select("+password");
-
+  
     if (!user) {
-        return next(new ErrorHandler("Invalid Email or Password", 401));
+      return next(new ErrorHandler("Invalid email or password", 401));
     }
-    const isPasswordMatched = user.comparePassword(password);
-
+  
+    const isPasswordMatched = await user.comparePassword(password);
+  
     if (!isPasswordMatched) {
-        return next(new ErrorHandler("Invalid Email or Password", 401));
+      return next(new ErrorHandler("Invalid email or password", 401));
     }
-
+  
     sendToken(user, 200, res);
-})
+  });
 
 // Logout User
 
@@ -82,16 +85,16 @@ exports.forgotpassword = catchAsyncErrors(async (req, res, next) => {
 
         await sendEmail({
             email: user.email,
-            subject:`Ecommerce Password Recovery`,
+            subject: `Ecommerce Password Recovery`,
             message,
 
         });
 
         res.status(200).json({
-            success:true,
-            message:`Email send to ${user.email} successfully`
+            success: true,
+            message: `Email send to ${user.email} successfully`
         })
-        
+
     } catch (error) {
         user.resetPasswordToken = undefined
         user.resetPasswordExpire = undefined
@@ -102,3 +105,49 @@ exports.forgotpassword = catchAsyncErrors(async (req, res, next) => {
 
 });
 
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    // creating token hash
+    const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() },
+    });
+
+    if (!user) {
+        return next(
+            new ErrorHandler(
+                "Reset Password Token is invalid or has been expired",
+                400
+            )
+        );
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not matched!", 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+});
+
+
+// Get User Details
+
+exports.getUserDetails = catchAsyncErrors(async(req, res, next) =>{
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success:true,
+        user
+    });
+});
