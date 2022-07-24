45a912a4d419ce43ea1c530295208f29291832d1
devbobi <md.saifurrahaman.cse@gmail.com>
2022-07-25 02:21:02 +0600

    orders handled

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
@@ -8,5 +8,14 @@ router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router
+    .route("/admin/orders")
+    .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+    .route("/admin/order/:id")
+    .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+    .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 module.exports = router;
