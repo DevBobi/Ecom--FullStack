4010a3ff79090616a3560995d9e2637fb84e3def
devbobi <md.saifurrahaman.cse@gmail.com>
2022-08-04 09:26:50 +0600

    home fixed

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaArrowCircleRight } from 'react-icons/fa'
 
 
 const Home = () => {
@@ -8,22 +9,26 @@ const Home = () => {
         <div className="relative w-full h-full flex justify-center bg-gradient-to-t from-black via-transparent to-gray-900 overflow-hidden">
 
             {/* :HERO IMAGE */}
-            <img src="https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" className="backdrop-blur-sm absolute w-full h-full object-cover object-left sm:object-center opacity-70 " />
+            <img src="https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" className="absolute w-full h-full object-cover object-left sm:object-center opacity-90 brightness-90" />
 
             {/* :HERO MAIN CONTAINER */}
-            <div className="relative py-28 xl:py-40 w-full md:w-2/3 lg:w-1/2 h-full flex flex-col justify-center">
+            <div className="relative py-28 xl:py-48 w-full md:w-2/3 lg:w-1/2 h-full flex flex-col justify-center">
 
                 {/* ::Hero title & text */}
                 <div className="mx-5 md:mx-0 p-8 rounded-xl md:rounded-r-none bg-gray-800 bg-opacity-50 text-white shadow-2xl">
-                    <h1 className="text-3xl sm:text-5xl font-josefin font-extrabold">Ecommerce, <br /><span className="text-yellow-400 text-opacity-80">Fancy Hero 8</span> est <br />Formidable !</h1>
+                    <h1 className="text-3xl sm:text-5xl font-josefin font-extrabold">Ecom, Fancy <br /><span className="text-yellow-400 text-opacity-80">Ecom</span> Website <br /></h1>
                     <p className="mt-3 text-gray-100 font-firacode">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta blanditiis in asperiores optio est, id.</p>
+
+
+                    {/* ::Hero button */}
+                    <button className="relative m-5 w-full md:w-2/3 lg:w-2/5 inline-flex items-center text-2xl font-firacode text-white overflow-hidden transition-all duration-300 transform hover:translate-x-4">
+                        <Link to="/" className="relative -top-0.5 mr-2 text-white ">Explore</Link>
+                        <FaArrowCircleRight />
+                        {/* <img src="https://fancytailwind.com/static/53e4540ef179fe51d5a02619660a1a92/arrow1.png" alt="" /> */}
+                    </button>
+
                 </div>
 
-                {/* ::Hero button */}
-                <button className="relative m-5 w-full md:w-2/3 lg:w-2/5 inline-flex items-center text-2xl font-firacode text-white overflow-hidden transition-all duration-300 transform hover:translate-x-4">
-                    <Link to="/" className="relative -top-0.5 mr-2 text-gray-900 ">Explore</Link>
-                    <img src="https://fancytailwind.com/static/53e4540ef179fe51d5a02619660a1a92/arrow1.png" alt="" className="absolute left-32 w-20" />
-                </button>
 
             </div>
         </div>
